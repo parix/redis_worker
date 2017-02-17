5bfc4186bd9f17537e884ae9c0e72bbdec352f29
Chris or Allen Rosario <c.allen.rosario@gmail.com>
2017-02-16 18:34:47 -0800

    Fixed redis call to push on queue.

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -6,7 +6,7 @@ redis = Redis.new(:host => "redis")
 id = 0
 while(true)
   puts "publishing: #{id}"
-  redis.queue "channel", { "hello_world" => id }
+  redis.rpush "channel", { "hello_world" => id }
   redis.publish "channel", "job: #{id}"
   id += 1
   sleep(1)
