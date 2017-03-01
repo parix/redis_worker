1a0de58c6530f45a43354a3c354025125928fcb7
Allen Rosario <c.allen.rosario@gmail.com>
2017-03-01 15:51:35 -0800

    Added ability to store and retrive results.

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -13,22 +13,32 @@ class Job
   end
 
   def queue
-    redis.lpush(@name, { "id" => @id, "args" => @args }.to_json)
-    redis.publish(@name, "Queueing Job id: #{@id}")
+    redis.lpush("#{@name}_jobs", { "id" => @id, "args" => @args }.to_json)
+    redis.publish("#{@name}_events", "Queueing Job id: #{@id}")
   end
 
   def redis
     @redis ||= Redis.new(:host => "redis")
   end
 
+  def result
+    hredis.hget("#{@name}_results", @id)
+  end
+
   def self.create(name:, args:)
     job = self.new(:name => name, :args => args)
     job.queue
+    job
   end
 end
 
 models = ["lebesgue_a_autodeny", "lebesgue_a_full_model", "lebesgue_b_autodeny", "lebesgue_b_full_model"]
-while(true)
-  Job.create(:name => "r_model", :args => [models.sample, {}])
-  sleep(1 + rand(2))
+
+jobs = models.map do |model|
+  Job.create(:name => "r_model", :args => [model, {}])
 end
+
+while(jobs.map(&:result).compact.count < 4)
+end
+
+puts jobs.map(&:result).compact
--- lib/worker.rb
+++ lib/worker.rb
@@ -3,6 +3,8 @@ require 'json'
 $stdout.sync = true
 
 class Worker
+  @@name = "default"
+
   def pubsub
     @pubsub ||= Redis.new(:host => "redis")
   end
@@ -11,20 +13,27 @@ class Worker
     @queue ||= Redis.new(:host => "redis")
   end
 
+  def start
+    pubsub.subscribe("#{@@name}_events") do |on|
+      on.message do |channel, message|
+        dequeue
+      end
+    end
+  end
+
   def dequeue
-    if job = queue.rpop("r_model")
+    if job = queue.rpop("#{@@name}_jobs")
       job = JSON.parse(job)
       puts "Working on Job id: #{job["id"]} with args: #{job["args"]}"
+      job["result"] = process(*job["args"])
+      puts "Finished Job id: #{job["id"]} result: #{job["result"]}"
+      queue.hset("#{@@name}_results", job["id"].to_s, job.to_json)
     end
   end
 
   def self.start
     worker = self.new
-    worker.pubsub.subscribe("r_model") do |on|
-      on.message do |channel, message|
-        worker.dequeue
-      end
-    end
+    worker.start
   end
 end
 
@@ -33,6 +42,23 @@ class RModel < Worker
 
   def process(model, data)
     puts "Scoring #{model} with #{data}"
+    case model
+    when "lebesgue_a_autodeny"
+      sleep(2)
+      91.12
+    when "lebesgue_a_full_model"
+      sleep(17)
+      68.49
+    when "lebesgue_b_autodeny"
+      sleep(2)
+      95.92
+    when "lebesgue_b_full_model"
+      sleep(17)
+      84.58
+    else
+      sleep(5)
+      0.00
+    end
   end
 end
 
